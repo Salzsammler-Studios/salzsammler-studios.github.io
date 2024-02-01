2052714f931d4a1072bc7cf8666162c11f1bd300
Ruben <56798357+DerBrezel@users.noreply.github.com>
2024-02-01 14:11:24 +0100

    optimize iamge for smaller laptops

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -4,13 +4,13 @@ import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 const Homepage = () => {
     return <Box minH={"105vh"}>
-        <Box pointerEvents={"none"} marginLeft={"-18rem"} marginTop={"-25rem"} bgSize={"100%"}width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloudsTogether.png')"} bgRepeat={"no-repeat"} bgPosition={"left top"}></Box>
+        <Box pointerEvents={"none"} marginLeft={"-15rem"} marginTop={"-21rem"} bgSize={"100%"}width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloudsTogether.png')"} bgRepeat={"no-repeat"} bgPosition={"left top"}></Box>
         <Box pointerEvents={"none"} marginLeft={"-0.1rem"} position={"absolute"} width={"100%"} height={"100%"} bgImage={"url('/layout2.svg')"} bgPosition="left bottom" bgRepeat="no-repeat" bgSize="50%"/>
         <SimpleGrid columns={2} spacing={10}>
             <Box></Box>
             <Box></Box>
             <Box></Box>
-            <Stack marginTop={"16rem" }maxW="103%" marginLeft={["-3rem","-3rem"]} paddingRight={["2rem","3rem"]}>
+            <Stack marginTop={"16rem" }maxW="110%" marginLeft={["-3rem","-4.5rem"]} paddingRight={["2rem","3rem"]}>
                 <Heading fontSize={["sm", "3xl"]} marginBottom={"-1rem"} sx={{fontWeight: 600}}>Interaktive Installationen</Heading>
                 <Heading fontSize={["lg", "8xl"]} sx={{ fontWeight: 900 }}>Salzsammler</Heading>
 
