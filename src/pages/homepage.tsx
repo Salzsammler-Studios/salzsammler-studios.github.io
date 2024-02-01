042ad341376c23b1d6f310017193e6c3a657cd94
Ruben <56798357+DerBrezel@users.noreply.github.com>
2024-02-01 14:00:48 +0100

    feat: revamp landing page

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -4,18 +4,18 @@ import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 const Homepage = () => {
     return <Box minH={"105vh"}>
-        <Box pointerEvents={"none"} marginLeft={"-15rem"} marginTop={"-20rem"} width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloudsTogether.png')"} bgRepeat={"no-repeat"} bgPosition={"left top"}></Box>
+        <Box pointerEvents={"none"} marginLeft={"-18rem"} marginTop={"-20rem"} bgSize={"110%"}width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloudsTogether.png')"} bgRepeat={"no-repeat"} bgPosition={"left top"}></Box>
         <Box pointerEvents={"none"} marginLeft={"-0.1rem"} position={"absolute"} width={"100%"} height={"100%"} bgImage={"url('/layout2.svg')"} bgPosition="left bottom" bgRepeat="no-repeat" bgSize="50%"/>
         <SimpleGrid columns={2} spacing={10}>
             <Box></Box>
             <Box></Box>
             <Box></Box>
-            <Stack marginTop={"18rem"}maxW="90%" marginLeft={["-3rem",0]} paddingRight={["2rem",0]}>
-                <Heading fontSize={["sm", "3xl"]}>Interaktive Installationen</Heading>
-                <Heading fontSize={["lg", "7xl"]} sx={{ fontWeight: 900 }}>Salzsammler</Heading>
+            <Stack marginTop={"20rem" }maxW="103%" marginLeft={["-3rem","-3rem"]} paddingRight={["2rem","3rem"]}>
+                <Heading fontSize={["sm", "3xl"]} marginBottom={"-1rem"} sx={{fontWeight: 600}}>Interaktive Installationen</Heading>
+                <Heading fontSize={["lg", "8xl"]} sx={{ fontWeight: 900 }}>Salzsammler</Heading>
 
                 <Text maxW="90%" fontSize={["sm", "2xl"]}>Salzsammler Studios fokussiert sich auf Innovationen im Bereich der Extended Reality, um didaktisch wertvolle Informationen interaktiv und spielbar erfahrbar zu machen.</Text>
-                <Text as="u" color="highlight"><Text as="a" href="#" sx={{ fontWeight: 600 }}>Mehr erfahren</Text><ArrowForwardIcon /></Text>
+                <Text as="u" color="highlight"><Text as="a" href="#" fontSize={["sm", "2xl"]} sx={{ fontWeight: 600 }}>Mehr erfahren</Text><ArrowForwardIcon /></Text>
             </Stack>
         </SimpleGrid>
     </Box>
