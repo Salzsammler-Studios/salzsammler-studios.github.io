22ef61715b9232f39a37522cdfa2c81757f2b020
Ruben <56798357+DerBrezel@users.noreply.github.com>
2024-02-06 21:30:00 +0100

    texte?

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,11 +1,13 @@
 import { Box, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react";
 
 const Project = () => {
+    const leftText = "Kinder werden nicht nur zum Spielen angeregt, sondern auch dazu, historische Themen zu erforschen und ein tiefes Verständnis für kulturelle Zusammenhänge zu entwickeln. Durch die enge Zusammenarbeit mit Experten aus Kulturinstitutionen entsteht eine immersive Lernumgebung, die Kinder für Geschichte begeistert und ihnen einen interaktiven Zugang zu historischen Inhalten bietet."
+    const rightText = "Salzsammler nutzt einen Projektor, um eine immersive und interaktive Projektionsfläche zu erschaffen, auf der Spielende und Würfel von der Kamera erfasst werden und somit nahtlos in die Anwendung integriert werden. Darüber hinaus setzt die Anwendung modernste Computer Vision-Technologien ein, um einen flüssigen Ablauf und eine beeindruckende Nutzererfahrung sicherzustellen."
     return (
         <Box>
             <Grid padding={"5rem"} gap={10} marginTop={["2rem", "4rem"]} templateColumns='repeat(3, 1fr)'>
                 <GridItem colSpan={3}><Heading size={["xs", "2xl"]} style={{ fontWeight: 600 }} color="highlight" w="80%">Eine interaktive Installation, die es Kindern ermöglicht, geschichtlichen Inhalten und Materialien intuitiv und mit Spielspaß zu begegnen.</Heading></GridItem>
-                <GridItem colSpan={3}>
+                <GridItem colSpan={3} marginBottom={["5rem"]}>
                     <Box
                         as='iframe'
                         src='https://www.youtube-nocookie.com/embed/grhau10Lkio?si=bMaPeC31GLPXVpMz?frameborder=0'
@@ -13,25 +15,25 @@ const Project = () => {
                         allowFullScreen
                         width='100%'
                         sx={{
-                            aspectRatio: '16/9'
+                            aspectRatio: '16/9',
+                            borderRadius: '63',
                         }}
                     />
                 </GridItem>
                 <GridItem colSpan={1}><Image src="horse.png" marginTop={["0", "-10rem"]} /></GridItem>
                 <GridItem colSpan={2} alignSelf="center"><Heading size={["xs", "xl"]} color="highlight">Der Handel und Transport von Waren stehen im Mittelpunkt. Kinder übernehmen Aufgaben wie das Annehmen von Baumaterialien, Lebensmitteln und Luxusgütern</Heading></GridItem>
                 <GridItem colSpan={1}></GridItem>
-                <GridItem colSpan={1}><Text>Der Handel und Transport von Waren stehen im Mittelpunkt. Kinder übernehmen Aufgaben wie das Annehmen von Baumaterialien, Lebensmitteln und Luxusgütern. Die Installation besteht aus Spielflächen, einem Projektor, einer Kamera, Würfeln und einer Recheneinheit.</Text></GridItem>
-                <GridItem colSpan={1}><Text>Der Fluss: Hier interagieren bis zu 3 Spieler mit Booten, verschieben sie und agieren auf der größten Spielfläche.
-                    Die Stadt und Brücke (Passiv): Diese Spielflächen vermitteln wertvolle Informationen über historische Ereignisse ohne interaktive Elemente.</Text></GridItem>
+                <GridItem colSpan={1}><Text>{leftText}</Text></GridItem>
+                <GridItem colSpan={1}><Text>{rightText}</Text></GridItem>
             </Grid>
-            <Grid padding={"5rem"} gap={10} templateColumns='repeat(3, 1fr)' backgroundColor={"highlight"}>
+            {/*<Grid padding={"5rem"} gap={10} templateColumns='repeat(3, 1fr)' backgroundColor={"highlight"}>
                 <GridItem colSpan={1}><Heading size={["xs", "2xl"]} style={{ fontWeight: 600 }} color="background" w="80%">Kamera</Heading></GridItem>
                 <GridItem colSpan={1}><Heading size={["xs", "2xl"]} style={{ fontWeight: 600 }} color="background" w="80%">Projektor</Heading></GridItem>
                 <GridItem colSpan={1}><Heading size={["xs", "2xl"]} style={{ fontWeight: 600 }} color="background" w="80%">Würfel</Heading></GridItem>
                 <GridItem colSpan={1}><Text color="background" w="80%">Die Kamera wird zentral über der Spielfläche installiert. Sie dient als Sensor, um der Anwendung zu ermöglichen, Spielende und Würfel zu erfassen.</Text></GridItem>
                 <GridItem colSpan={1}><Text color="background" w="80%">Der Projektor wird über der Spielfläche installiert und ist für die Erzeugung der interaktiven Projektionsfläche verantwortlich. Das zu projizierende Bild erhält der Projektor durch die Recheneinheit.</Text></GridItem>
                 <GridItem colSpan={1}><Text color="background" w="80%">Die Würfel sind Spielobjekte mit verschiedenen Symbolen auf ihren Seitenflächen, die von den Spielenden platziert und rotiert werden können, um den Zustand der Anwendung zu beeinflussen. Je nach Ausrichtung und Position der Symbole können diese über das Bild der Kamera erfasst und an die Anwendung weitergeleitet und dort weiterverarbeitet werden.</Text></GridItem>
-            </Grid>
+                    </Grid>*/}
         </Box>
     );
 }
