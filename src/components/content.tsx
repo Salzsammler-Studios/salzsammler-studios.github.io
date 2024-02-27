7af06793760d992e2b690e502668867f182fd1f5
Ruben <56798357+DerBrezel@users.noreply.github.com>
2024-02-27 19:38:47 +0100

    feat: initial draft mobile

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import { Box, Drawer, DrawerBody, DrawerContent, DrawerOverlay, IconButton, Link, MenuIcon, Tab, TabList, TabPanel, TabPanels, Tabs, VStack, useDisclosure } from "@chakra-ui/react";
 import Homepage from "../pages/homepage";
 import Contact from "../pages/contact";
 import Logo from "./logo";
@@ -6,11 +6,16 @@ import AboutUs from "../pages/about_us";
 import Project from "../pages/project";
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useMediaQuery } from "react-responsive";
+import { HamburgerIcon, SunIcon } from "@chakra-ui/icons";
 
 type ContentType = {
     initialTabIndex: number
 }
 const Content = ({ initialTabIndex }: ContentType) => {
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    
     const colors: string[] = ["background", 'background', 'background', 'highlight']
     const [tabIndex, setTabIndex] = useState(0)
     const bg = colors[tabIndex]
@@ -61,27 +66,71 @@ const Content = ({ initialTabIndex }: ContentType) => {
     }, [location.pathname, initialTabIndex]);
 
     return (
-        <Box bg={bg} bgSize={"cover"}>
-            <Tabs index={initialTabIndex} isLazy align='end' position="relative" variant='unstyled' size={['sm', 'lg']} onChange={handleTabChange}>
-                <TabList sx={{ marginBottom: ["-7.5rem", "-8.5rem"] }} padding={"2rem"} paddingRight={["0rem","3rem"]}>
-                    <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Home</Tab>
-                    <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Projekt</Tab>
-                    <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Team</Tab>
-                    <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Kontakt</Tab>
-                </TabList>
-                <TabPanels textAlign="start">
-                    <TabPanel sx={{ margin: 0, padding: 0 }}><Homepage /></TabPanel>
-                    <TabPanel sx={{ margin: 0, padding: 0 }}>
-                        <Logo color='black' />
-                        <Project /></TabPanel>
-                    <TabPanel sx={{ margin: 0, padding: 0 }} >
-                        <Logo color='black' />
-                        <AboutUs /></TabPanel>
-                    <TabPanel sx={{ margin: 0, padding: 0 }}><Logo color='background' /><Contact /></TabPanel>
-                </TabPanels>
-            </Tabs>
-        </Box>
+        <Box bg={bg} bgSize="cover" position="relative">
+        {isMobile ? (
+          <>
+            <IconButton
+              icon={<HamburgerIcon />}
+              aria-label="Menu"
+              onClick={onOpen}
+              variant="ghost"
+              zIndex={100}
+              borderRadius="50%"
+              position="absolute"
+              top="3rem"
+              right="1rem"
+            />
+            <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+            <DrawerOverlay />
+        <DrawerContent>
+          <DrawerBody>
+            <VStack spacing={4}>
+              <Link href="#/" onClick={onClose}>Home</Link>
+              <Link href="#/project" onClick={onClose}>Projekt</Link>
+              <Link href="#/team" onClick={onClose}>Team</Link>
+              <Link href="#/contact" onClick={onClose}>Kontakt</Link>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+      {/* Render TabPanels for mobile */}
+      <Tabs index={initialTabIndex}>
+      <TabPanels textAlign="start">
+        <TabPanel sx={{ margin: 0, padding: 0 }}><Homepage /></TabPanel>
+        <TabPanel sx={{ margin: 0, padding: 0 }}>
+          <Logo color='black' />
+          <Project />
+        </TabPanel>
+        <TabPanel sx={{ margin: 0, padding: 0 }} >
+          <Logo color='black' />
+          <AboutUs />
+        </TabPanel>
+        <TabPanel sx={{ margin: 0, padding: 0 }}><Logo color='background' /><Contact /></TabPanel>
+      </TabPanels>
+      </Tabs>
+          </>
+        ) : (
+          <Tabs index={initialTabIndex} isLazy align='end' position="relative" variant='unstyled' size={['sm', 'lg']} onChange={handleTabChange}>
+            <TabList sx={{ marginBottom: ["-7.5rem", "-8.5rem"] }} padding={"2rem"} paddingRight={["0rem","3rem"]}>
+              <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Home</Tab>
+              <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Projekt</Tab>
+              <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Team</Tab>
+              <Tab color={tabIndex === 3 ? 'background' : 'black'} _selected={{ opacity: 1, fontSize: ['sm', '3xl'] }} sx={{ fontWeight: 600, opacity: 0.5 }} fontSize={['xs', '2xl']}>Kontakt</Tab>
+            </TabList>
+            <TabPanels textAlign="start">
+              <TabPanel sx={{ margin: 0, padding: 0 }}><Homepage /></TabPanel>
+              <TabPanel sx={{ margin: 0, padding: 0 }}>
+                <Logo color='black' />
+                <Project /></TabPanel>
+              <TabPanel sx={{ margin: 0, padding: 0 }} >
+                <Logo color='black' />
+                <AboutUs /></TabPanel>
+              <TabPanel sx={{ margin: 0, padding: 0 }}><Logo color='background' /><Contact /></TabPanel>
+            </TabPanels>
+          </Tabs>
+        )}
+      </Box>
     );
-};
+  };
 
 export default Content;
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,26 +1,57 @@
-import { Box, Text, Heading, Stack, SimpleGrid } from '@chakra-ui/react';
+import {useNavigate} from 'react-router-dom';
+import { Box, Text, Heading, Stack, SimpleGrid, VStack } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 import homepage_background from '../assets/homepage_background.svg';
+import { useMediaQuery } from 'react-responsive';
 
 const Homepage = () => {
-    return <Box minH={"105vh"}>
-        <Box pointerEvents={"none"} bgSize={"25%"} width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloud_small.svg')"} bgRepeat={"no-repeat"} bgPosition={"left top"} style={{shapeRendering: "crispEdges"}}></Box>
-        <Box pointerEvents={"none"} marginLeft={"-0.1rem"} position={"absolute"} width={"100%"} height={"100%"} bgImage={homepage_background} bgPosition="left bottom" bgRepeat="no-repeat" bgSize={["90%","50%"]} style={{shapeRendering: "crispEdges"}}/>
-        <Box pointerEvents={"none"} bgSize={"60%"} width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloud_big.svg')"} bgRepeat={"no-repeat"} bgPosition={"left top"} style={{shapeRendering: "crispEdges"}}></Box>
-        
-        <SimpleGrid columns={2} spacing={10}>
-            <Box></Box>
-            <Box></Box>
-            <Box></Box>
-            <Stack marginTop={["10rem","16rem"]}maxW="110%" marginLeft={["-1rem","4rem"]} paddingRight={["2rem","3rem"]}>
-                <Heading fontSize={["sm", "3xl"]} marginBottom={["-0.5rem","-1.25rem"]} sx={{fontWeight: 600}}>Interaktive Installationen</Heading>
-                <Heading fontSize={["xl", "7xl"]} sx={{ fontWeight: 900 }}>Salzsammler</Heading>
-
-                <Text marginTop={["0rem","-0.7rem"]}maxW={["100%","90%"]} fontSize={["sm", "2xl"]} lineHeight={["","9"]}>Salzsammler Studios fokussiert sich auf Innovationen im Bereich der Extended Reality, um didaktisch wertvolle Informationen interaktiv und spielbar erfahrbar zu machen.</Text>
-                <Text as="u" color="highlight" marginTop={"1rem"}><Text as="a" href="#/project" fontSize={["sm", "2xl"]} sx={{ fontWeight: 600 }}>Mehr erfahren</Text><ArrowForwardIcon /></Text>
-            </Stack>
-        </SimpleGrid>
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+    return <Box>
+        {isMobile ? <MobileHomepage /> : <DesktopHomepage />}
     </Box>
 }
+    export default Homepage;
 
-export default Homepage;
+
+const DesktopHomepage = () => {
+    return <Box minH={"105vh"}>
+    <Box pointerEvents={"none"} bgSize={"25%"} width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloud_small.svg')"} bgRepeat={"no-repeat"} bgPosition={"left top"} style={{ shapeRendering: "crispEdges" }}></Box>
+    <Box pointerEvents={"none"} marginLeft={"-0.1rem"} position={"absolute"} width={"100%"} height={"100%"} bgImage={homepage_background} bgPosition="left bottom" bgRepeat="no-repeat" bgSize={["90%", "50%"]} style={{ shapeRendering: "crispEdges" }} />
+    <Box pointerEvents={"none"} bgSize={"60%"} width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/cloud_big.svg')"} bgRepeat={"no-repeat"} bgPosition={"left top"} style={{ shapeRendering: "crispEdges" }}></Box>
+
+    <SimpleGrid columns={2} spacing={10}>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Stack marginTop={["10rem", "16rem"]} maxW="110%" marginLeft={["-1rem", "4rem"]} paddingRight={["2rem", "3rem"]}>
+            <Heading fontSize={["sm", "3xl"]} marginBottom={["-0.5rem", "-1.25rem"]} sx={{ fontWeight: 600 }}>Interaktive Installationen</Heading>
+            <Heading fontSize={["xl", "7xl"]} sx={{ fontWeight: 900 }}>Salzsammler</Heading>
+
+            <Text marginTop={["0rem", "-0.7rem"]} maxW={["100%", "90%"]} fontSize={["sm", "2xl"]} lineHeight={["", "9"]}>Salzsammler Studios fokussiert sich auf Innovationen im Bereich der Extended Reality, um didaktisch wertvolle Informationen interaktiv und spielbar erfahrbar zu machen.</Text>
+            <Text as="u" color="highlight" marginTop={"1rem"}><Text as="a" href="#/project" fontSize={["sm", "2xl"]} sx={{ fontWeight: 600 }}>Mehr erfahren</Text><ArrowForwardIcon /></Text>
+        </Stack>
+    </SimpleGrid>
+</Box>
+}
+
+const MobileHomepage = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+      navigate("/project");
+    };
+
+    return <Box minH={"105vh"}>
+         <Box marginTop={"-3rem"} pointerEvents={"none"} bgSize={"80%"} width={"100%"} height={"100%"} position={"absolute"} bgImage={"url('/mobile_clouds.svg')"} bgRepeat={"no-repeat"} bgPosition={"left top"} style={{ shapeRendering: "crispEdges" }}></Box>
+         <Box marginTop={"1rem"} zIndex={2} pointerEvents={"none"} bgSize={"80%"} width={"101%"} height={"101%"} position={"absolute"} bgImage={"url('/mobile_town.svg')"} bgRepeat={"no-repeat"} bgPosition={"right bottom"} bottom={0}style={{ shapeRendering: "crispEdges" }}></Box>
+        <VStack>
+            <Box marginTop={"16rem"} pointerEvents={"none"}></Box>
+            <Heading fontSize={"md"} marginBottom={"-1.25rem"} sx={{ fontWeight: 600}}>Interaktive Installationen</Heading>
+            <Heading fontSize={ "4xl"} sx={{ fontWeight: 900}}>Salzsammler</Heading>
+            <Heading fontSize={ "4xl"} sx={{ fontWeight: 900}} marginTop={"-1.5rem"}>Studios</Heading>
+
+            <Text marginTop={"0.5rem"} maxW={"70%"} fontSize={"sm"} lineHeight={["", "9"]}>Salzsammler Studios fokussiert sich auf Innovationen im Bereich der Extended Reality, um didaktisch wertvolle Informationen interaktiv und spielbar erfahrbar zu machen.</Text>
+            <Box as="button" marginTop={"0.1rem"} onClick={handleClick} bg="highlight" color={"background"} borderRadius={"5px"} padding="0.6rem"><b>Mehr erfahren</b></Box>
+        </VStack>
+    </Box>
+}
