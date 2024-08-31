135615495e5201c6cabb4ffecedfef8a01c140f5
Ruben <56798357+DerBrezel@users.noreply.github.com>
2024-08-31 11:14:50 +0200

    remove john and adapt spacing

--- src/pages/about_us.tsx
+++ src/pages/about_us.tsx
@@ -7,15 +7,10 @@ const AboutUs = () => {
     const { t } = useTranslation();
     return (
         <Box>
-            <SimpleGrid columns={[1, 3]} alignItems={"baseline"} justifyContent={"center"} columnGap={"2rem"} paddingLeft={"5vw"} paddingRight={"4vw"} >
-                <Box height={["2rem", "5rem"]}></Box>
-                <Box height={["2rem", "5rem"]}></Box>
-                <Box height={["2rem", "5rem"]}></Box>
-                <Box></Box>
-                <Heading size="3xl" textAlign="center" sx={{ fontWeight: 600 }} marginBottom="3rem">{t('about_us.header')}</Heading>
-                <Box></Box>
+            <Box height={["2rem", "5rem"]}></Box>
+            <Heading size="3xl" textAlign="center" sx={{ fontWeight: 600 }} marginBottom="3rem" columnGap={"2rem"} paddingLeft={"5vw"} paddingRight={"4vw"}>{t('about_us.header')}</Heading>
+            <SimpleGrid columns={[1, 2]} alignItems={"baseline"} justifyContent={"center"} columnGap={"2rem"} paddingLeft={"5vw"} paddingRight={"4vw"} >
                 <TeamCards image='/ruben.png' name='Ruben Schäfer' title='DEVELOPER' text={t('about_us.rubentext')} />
-                <TeamCards image='/john.png' name='John Beinecke' title='PRODUCER' text={t('about_us.johntext')} />
                 <TeamCards image='/nic.png' name='Nic Schilling' title='GAME DESIGNER' text={t('about_us.nictext')} />
             </SimpleGrid>
             <AboutUsFooter style={{ display: "flex", width: "100%", height: "auto", zIndex: "2" }} title="Eine Stadt in der Installation Salzsammler" />
